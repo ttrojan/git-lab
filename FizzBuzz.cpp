3e36a40f36838f1d3b54dac79156eb5a6b5f8c54
Rami Al-Ghanmi <rami@ramialghanmi.com>
2014-04-03 18:15:44 -0700

    Added rule for Buzz

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    for(unsigned int i = 0; i < 100; i++) {
-        std::cout << i << std::endl;
-    }
+	for(unsigned int i = 0; i < 100; i++) {
+		if( i % 5 == 0)
+			std::cout << i << ": " << "Buzz" << std::endl;
+	}
 
-    return(0);
+	return(0);
 }
